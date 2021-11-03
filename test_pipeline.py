739cb554b93351fca1abb92357f76a714deaa519
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2021-11-03 10:05:50 +0000

    changed to probabilities

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,8 +1,9 @@
 def test(model, test_dataframe, features, target_variable):
     X_test = test_dataframe[features]
-    y_pred = model.predict(X_test)
 
-    test_dataframe[target_variable] = y_pred
+    probs = model.predict_proba(X_test)[:, 1]
+
+    test_dataframe[target_variable] = probs
     submission = test_dataframe[["loan_id", "status"]]
     submission = submission.rename(columns={"loan_id": "Id", "status": "Predicted"})
 
