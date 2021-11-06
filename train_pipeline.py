15b818fda0adf4e273034095baebb996e632ea4d
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2021-11-06 16:00:56 +0000

    submission with 0.61296

--- train_pipeline.py
+++ train_pipeline.py
@@ -7,21 +7,22 @@ def train(dataframe, features, target_variable):
     X = dataframe[features]
     y = dataframe[target_variable]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
     print("Training data...")
 
     # Create Decision Tree classifier object
-    clf = DecisionTreeClassifier()
+    clf = DecisionTreeClassifier(min_samples_leaf=10)
 
     # Train Decision Tree Classifier
     clf = clf.fit(X_train, y_train)
 
     # Predict the response for test dataset
-    y_pred = clf.predict(X_test)
+    y_pred = clf.predict_proba(X_test)[:, 1]
 
-    # Model Accuracy, how often is the classifier correct?
-    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+    # Model Area Under the Curve, the higher the better
+    auc = metrics.roc_auc_score(y_test, y_pred)
+    print("AUC Score: ", auc)
 
-    # return the model
+    # return the model and the data used to test
     return clf
